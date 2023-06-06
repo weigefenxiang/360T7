4c86fd687f25f726a76a199185aa6fb9399a9eb9
weigefenxiang <119787190+weigefenxiang@users.noreply.github.com>
2023-06-06 14:32:06 +0800

    Do not be evil

--- Shell/diy2-360T7-padavanonly.sh
+++ Shell/diy2-360T7-padavanonly.sh
@@ -25,5 +25,5 @@ sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_g
 sed -i 's,mirrors.vsean.net/openwrt,mirrors.pku.edu.cn/immortalwrt,g'  package/emortal/default-settings/files/99-default-settings-chinese
 
 # Do not be evil # ae6ff34105444482cc3d46d43987cc467ea79ac7
-sed -i '1d' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
+LANG=C sed -i ':label;N;s/^[\x81-\xFE][\x40-\xFE].*\n//g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
 cat target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-h3c-nx30pro.dts
